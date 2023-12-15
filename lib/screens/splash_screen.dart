ab1f09d7c9fd6bd5f00071e6937f5721eb1bc619
ayoob <ayoobkibrahim01@gmail.com>
2023-12-15 09:12:49 +0530

    Commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
+
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-
-    
-    Future.delayed(Duration(seconds: 4), () {
+    Future.delayed(Duration(seconds: 10000), () {
     
       Navigator.of(context).pushReplacementNamed('/OnboardingPages'); 
     });
@@ -18,12 +17,35 @@ class SplashScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           Text("ChargeMOD",style:
-          
-            GoogleFonts.poppins(textStyle: Theme.of(context).textTheme.displayLarge,
-            fontSize: 30,),
-           ),
-           
+           RichText(
+             text: TextSpan(
+              children: [
+      TextSpan(
+        text: "charge",
+        style: 
+       
+        GoogleFonts.poppins(textStyle: Theme.of(context).textTheme.displayLarge,
+                    fontSize: 35,color: Colors.black,
+                     letterSpacing: 1.0
+            ),
+      ),
+      TextSpan(
+        text: "MOD",
+        style: GoogleFonts.poppins(textStyle: Theme.of(context).textTheme.displayLarge,
+                    fontSize: 35,color:Colors.orange,
+                     letterSpacing: 2.0
+              ),
+      ),
+    ],
+  ),
+),
+Text(
+  '                                             POWER TO PEOPLE',
+  style: 
+  GoogleFonts.poppins(textStyle: Theme.of(context).textTheme.displayLarge,
+                    fontSize: 11,color: Colors.orange,
+             ),
+),
             SizedBox(height: 20),
             
           ],
